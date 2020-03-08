c7d64517697d8ae8578af16e3665577c02da0445
Catarina Laranjeiro <ctlaranjeiro@gmail.com>
2020-03-08 11:15:30 +0000

    Done. Add a few bonus.

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var ctx = canvas.getContext('2d');
 var width = canvas.width;
 var height = canvas.height;
 
-// Iteration 1
+//----------- ITERATION 1 -----------
 function drawGrid() {
 
     //ctx.fillStyle = "blue";
@@ -34,7 +34,7 @@ function drawGrid() {
 }
 
 
-// ITERATION 2: Character
+//----------- ITERATION 2: Character -----------
 class Character {
     constructor (){
         this.col = 0;
@@ -45,26 +45,26 @@ class Character {
 
     //methods
     moveUp(){
-        this.row -= 1;
+        this.row -= 50;
     }
 
     moveRight(){
-        this.col += 1;
+        this.col += 50;
     }
 
     moveDown(){
-        this.row += 1;
+        this.row += 50;
     }
 
     moveLeft(){
-        this.col -= 1;
+        this.col -= 50;
     }
-};
+}
 
 let character = new Character();
 
 
-// ITERATION 3: drawPlayer function
+//----------- ITERATION 3: drawPlayer function -----------
 
 
 function drawPlayer (col, row){
@@ -73,13 +73,112 @@ function drawPlayer (col, row){
     ctx.drawImage(character.image, col, row, 50,50);
 }
 
+//----------- ITERATION 4: drawTreasure function -----------
 
+class Treasure {
+    constructor(col, row){
+        this.col = 50;
+        this.row = 50;
+        this.image = new Image();
+        this.image.src = "https://www.mariowiki.com/images/thumb/4/47/Super-gem.png/200px-Super-gem.png";
+    }
+
+    setRandomPosition(){
+        for (let hor = 0; hor <= canvas.width - 1; hor += 50){
+            let randomNum = Math.floor(Math.random()*hor);
+            this.col = Math.round(randomNum / 50)*50;
+        }
+
+        for (let ver = 0; ver <= canvas.height - 1; ver += 50){
+            let randomNum = Math.floor(Math.random()*ver);
+            this.row = Math.round(randomNum / 50)*50;
+        }        
+    }
+}
+
+let treasure = new Treasure();
+treasure.setRandomPosition();
+//console.log(treasure.col);
+
+function drawTreasure (col,row){
+    //treasure.setRandomPosition();
+    ctx.drawImage(treasure.image, col, row, 50, 50);
+}
 
 
 function drawEverything() {
   drawGrid();
   drawPlayer(character.col, character.row);
-  // drawTreasure()
+  drawTreasure(treasure.col, treasure.row);
 }
 
+
 window.addEventListener("load", drawEverything());
+
+
+//----------- ITERATION 5 -----------
+
+document.onkeydown = function(e) {
+    e.preventDefault(); // Stop the default behavior (moving the screen to the left/up/right/down)
+    switch (e.keyCode) {
+      case 37: 
+        // console.log('left');
+        if (character.col <= 0){
+            // console.log("it's out of the board")
+            break;
+        } else{
+            character.moveLeft();
+        }
+        
+        if(character.col === treasure.col && character.row === treasure.row){
+                treasure.setRandomPosition();
+            }
+        
+        break;
+      case 38: 
+        // console.log('up');
+        if (character.row <= 0){
+            // console.log("it's out of the board")
+            break;
+        } else{
+            character.moveUp(); 
+        }
+        
+        if(character.col === treasure.col && character.row === treasure.row){
+            treasure.setRandomPosition();
+        } 
+        
+        break;
+      case 39: 
+        // console.log('right');
+        if (character.col >= canvas.width - 50){
+            // console.log("it's out of the board")
+            break;
+        } else{
+            character.moveRight();
+        }
+        
+        if(character.col === treasure.col && character.row === treasure.row){
+            treasure.setRandomPosition();
+        }
+        
+        break;
+      case 40: 
+        // console.log('down');
+        if (character.row >= canvas.height - 50){
+            // console.log("it's out of the board")
+            break;
+        } else{
+            character.moveDown();
+        }
+        
+        if(character.col === treasure.col && character.row === treasure.row){
+            treasure.setRandomPosition();
+        }
+        
+        break;
+    }
+    ctx.clearRect (0,0, canvas.width, canvas.height);
+    drawEverything();
+    return e;
+};
